1db4a52d169e3b1a0657a05492f18192ea471313
Forrest <forrest.waters@wpengine.com>
2017-04-26 13:44:46 -0500

    Nuking the list within a list idea

--- milestones/tic-tac-toe.py
+++ milestones/tic-tac-toe.py
@@ -1,8 +1,11 @@
-def display_board():
+def game_board():
 	global board
-        board = [['1','2','3'],['4','5','6'],['7','8','9']]
-        print board[0]
-	print board[1]
-	print board[2]	
-display_board()
+        board = ['1','2','3','4','5','6','7','8','9']
+def display_board():
+	print board[0:3]
+	print board[3:6]
+	print board[6:9]
 
+game_board()
+
+display_board()
