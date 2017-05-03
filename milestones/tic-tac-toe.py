ad533481507cbcd4d37f81dabadb9c2d06b01b7b
Forrest <forrest.waters@wpengine.com>
2017-05-03 18:32:40 -0500

    This works. Need to build while loop to run game

--- milestones/tic-tac-toe.py
+++ milestones/tic-tac-toe.py
@@ -1,11 +1,82 @@
+import random
+
+players = [1, 2]
+players[0] = player_1_marker = 'X'
+players[1] = player_2_marker = 'O'
+
+"""Defining main game board for the game"""
 def game_board():
 	global board
         board = ['1','2','3','4','5','6','7','8','9']
+
+"""Function to display the current board"""
 def display_board():
 	print board[0:3]
 	print board[3:6]
 	print board[6:9]
 
-game_board()
+"""Coin flip to choose which player goes first. Assigns that player a boolean of true and the other player False"""
+def coin_flip():
+	global player_1_turn
+	global player_2_turn
+	turn = random.randint(1,2)
+	if turn == 1:
+		player_1_turn = True
+		player_2_turn = False
+	else:
+		player_1_turn = False
+		player_2_turn = True
 
-display_board()
+"""Section where we take players input and place their piece"""
+def players_turn():
+	if player_1_turn:
+		current_player = players[0]
+	else:
+		current_player = players[1]
+	global board
+	global players_choice
+	players_choice = raw_input("Please select an open space: ")
+	if players_choice.isdigit():
+		if 0 < int(players_choice) < 10:
+			for index in range(len(board)):
+				board_spaces = board[index]
+				if players_choice == board_spaces:
+					board = [w.replace(players_choice, current_player) for w in board]
+		else:
+			try_again()
+	else:
+		try_again()
+	status_check()
+
+
+"""Starts over with players_turn if the wrong value is provided"""
+def try_again():
+	print "Oops, try again."
+	players_turn()
+
+"""Checks to see if someone has won or if the board is full"""
+def status_check():
+	l = [player_1_marker, player_2_marker]
+	global player
+	for player in l:
+		global game_finished
+		game_finished = True
+		if board[0] == board[1] == board[2] == player:
+			congrats()
+		elif board[3] == board[4] == board[5] == player:
+			congrats()
+		elif board[6] == board[7] == board[8] == player:
+			congrats()
+		elif board[0] == board[3] == board[6] == player:
+			congrats()
+		elif board[1] == board[4] == board[7] == player:
+			congrats()
+		elif board[2] == board[5] == board[8] == player:
+			congrats()
+		elif board[0] == board[4] == board[8] == player:
+			congrats()
+		elif board[2] == board[4] == board[6] == player:
+			congrats()
+		else:
+			game_finished = False
+			players_turn()
